d34c83fd498c92c0262c80c42e2944b0c200546f
Gabriel <rodriguesgasoares@gmail.com>
2025-11-04 22:16:47 -0300

    wip: comecando a barra de navegacao no topo

--- src/app/core/layout/topnav/topnav.ts
+++ src/app/core/layout/topnav/topnav.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatIconModule, MatIconRegistry } from '@angular/material/icon'
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-topnav',
-  imports: [],
+  imports: [
+    MatIconModule
+  ],
   templateUrl: './topnav.html',
   styleUrl: './topnav.scss',
 })
 export class Topnav {
+  iconReg = inject(MatIconRegistry)
+  sanitizer = inject(DomSanitizer)
 
+  constructor() {
+    this.registerIcon();
+  }
+
+  protected registerIcon() {
+    this.iconReg.addSvgIcon(
+      'home',
+      this.sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/home.svg')
+    );
+    this.iconReg.addSvgIcon(
+      'projects',
+      this.sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/projects.svg')
+    );
+    this.iconReg.addSvgIcon(
+      'articles',
+      this.sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/articles.svg')
+    );
+    this.iconReg.addSvgIcon(
+      'about',
+      this.sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/about.svg')
+    );
+    this.iconReg.addSvgIcon(
+      'contact',
+      this.sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/contact.svg')
+    );
+  }
 }
