746783579ab0a0980c8cff99522a94a8c9334152
Gabriel <rodriguesgasoares@gmail.com>
2025-11-05 18:09:06 -0300

    wip: fazendo a home page

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,11 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [
+    MatIconModule,
+    RouterLink
+],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
 export class Home {
+iconReg = inject(MatIconRegistry)
+sanitizer = inject(DomSanitizer)
 
+  constructor() {
+    this.registerIcon();
+  }
+
+  protected registerIcon() {
+    this.iconReg.addSvgIcon(
+      'github',
+      this.sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/github.svg')
+    );
+    this.iconReg.addSvgIcon(
+      'linkedin',
+      this.sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/linkedin.svg')
+    );
+    this.iconReg.addSvgIcon(
+      'x',
+      this.sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/x.svg')
+    );
+    this.iconReg.addSvgIcon(
+      'spotify',
+      this.sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/spotify.svg')
+    );
+    this.iconReg.addSvgIcon(
+      'instagram',
+      this.sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/instagram.svg')
+    );
+  }
 }
