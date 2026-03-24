9b0703b7e9e7f2b521bc7c2821f920db62da57e9
Gabriel <rodriguesgasoares@gmail.com>
2026-03-24 17:33:43 -0300

    refactor: Migração para tailwind da tela de projetos

--- src/app/features/projects/projects.ts
+++ src/app/features/projects/projects.ts
@@ -2,6 +2,16 @@ import { Component, inject } from '@angular/core';
 import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
 
+
+interface Project {
+  name: string;
+  description: string;
+  tags: string[];
+  year: string;
+  status: 'concluído' | 'em andamento';
+  github: string;
+}
+
 @Component({
   selector: 'app-projects',
   imports: [
@@ -11,17 +21,46 @@ import { DomSanitizer } from '@angular/platform-browser';
   styleUrl: './projects.scss',
 })
 export class Projects {
-  iconReg = inject(MatIconRegistry)
-  sanitizer = inject(DomSanitizer)
+  private iconReg = inject(MatIconRegistry);
+  private sanitizer = inject(DomSanitizer);
 
-  constructor() {
-    this.registerIcon();
-  }
+  projects: Project[] = [
+    {
+      name: 'BPSentry',
+      description: 'Sistema de monitoramento com pipeline ETL e arquitetura Medallion.',
+      tags: ['Python', 'PostgreSQL', 'Pandas'],
+      year: '2026',
+      status: 'em andamento',
+      github: 'https://github.com/RodriguesGS/BPSentry'
+    },
+    {
+      name: 'F1Lake',
+      description: 'Sistema para predição de campeão da F1',
+      tags: ['Python', 'AWS', 'Streamlit'],
+      year: '2026',
+      status: 'em andamento',
+      github: 'https://github.com/RodriguesGS/F1Lake'
+    },
+    {
+      name: 'AIPredict',
+      description: 'Programa que irá prever e classificar status dos backups de empresas.',
+      tags: ['Python', 'Scikit-learn', 'Pandas'],
+      year: '2026',
+      status: 'concluído',
+      github: 'https://github.com/RodriguesGS/AIPredict'
+    },
+    {
+      name: 'WeatherETL',
+      description: 'ETL com Dados Meteorológicos de Maringá.',
+      tags: ['Python', 'Airflow', 'Docker', 'PostgreSQL'],
+      year: '2026',
+      status: 'concluído',
+      github: 'https://github.com/RodriguesGS/WeatherETL'
+    },
+  ];
 
-  protected registerIcon() {
-    this.iconReg.addSvgIcon(
-      'github',
-      this.sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/github.svg')
-    );
-  }
+  private _ = this.iconReg.addSvgIcon(
+    'github',
+    this.sanitizer.bypassSecurityTrustResourceUrl('/assets/icons/github.svg')
+  );
 }
