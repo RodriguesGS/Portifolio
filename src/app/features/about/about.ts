a0ef3b6a5d5d937b173ff30f91303a7025cf8d38
Gabriel <rodriguesgasoares@gmail.com>
2026-03-25 14:48:24 -0300

    refactor: Página Sobre remodelada

--- src/app/features/about/about.ts
+++ src/app/features/about/about.ts
@@ -1,11 +1,38 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
+interface Items {
+  icon: string;
+  label: string;
+  text: string;
+}
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [ ],
   templateUrl: './about.html',
   styleUrl: './about.scss',
 })
 export class About {
-
+  items: Items[] = [
+    {
+      icon: '🎯',
+      label: 'Foco Atual',
+      text: 'Aprofundando em <strong style="color:#E6E6E6;font-weight:500">Engenharia de Dados</strong> — pipelines, modelagem e automação com Python e PostgreSQL.'
+    },
+    {
+      icon: '🔨',
+      label: 'Construindo',
+      text: '<strong style="color:#E6E6E6;font-weight:500">BPSentry</strong> — sistema de monitoramento de backups com arquitetura Medallion e ETL automatizado.'
+    },
+    {
+      icon: '📚',
+      label: 'Aprendendo',
+      text: '<strong style="color:#E6E6E6;font-weight:500">Machine Learning</strong> aplicado a dados reais, análise estatística e visualização com Power BI.'
+    },
+    {
+      icon: '🌍',
+      label: 'Objetivo',
+      text: 'Me tornar um <strong style="color:#E6E6E6;font-weight:500">Engenheiro de Dados</strong> criando soluções que transformem dados em decisões reais.'
+    },
+  ];
 }
